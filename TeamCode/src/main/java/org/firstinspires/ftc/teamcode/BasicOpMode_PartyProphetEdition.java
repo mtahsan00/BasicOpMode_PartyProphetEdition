5ca0320e97d2828f9ac8e94efd15626cf2c6761b
Muhammad Ahsan <mtahsan00@gmail.com>
2016-10-10 01:02:38 -0500

    Initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOpMode_PartyProphetEdition.java
@@ -0,0 +1,155 @@
+/*
+Copyright (c) 2016 Robert Atkinson
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted (subject to the limitations in the disclaimer below) provided that
+the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list
+of conditions and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright notice, this
+list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+Neither the name of Robert Atkinson nor the names of his contributors may be used to
+endorse or promote products derived from this software without specific prior
+written permission.
+
+NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESSFOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * This file contains an example of an iterative (Non-Linear) "OpMode".
+ * An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
+ * The names of OpModes appear on the menu of the FTC Driver Station.
+ * When an selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a PushBot
+ * It includes all the skeletal structure that all iterative OpModes contain.
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+
+/**cuz I'm being lazy, I pasted the base the code from TEMPLATEOpMode_Iterative as per FTC instructions.
+ * But fear not I did modify the code and add comments not only to enhance user experience, but also
+ * to provide maximum information for your young-tiny minds. Don't worry this won't be your run-of-the-mill
+ * boring code. This is going to be the MOST interesting code you'll ever read. I've written
+ * this code using JAKE JENSEN standards, so you know it'll be high quality.
+ * CHALLENGE: FIND HIDDEN CODE AND ACTIVATE. (IT ISN'T HARD TO FIND, I DID THAT ON PURPOSE FOR YOU BEGINNERS)
+ */
+
+/**I dedicate this code to LINUX creator Linux Torvalds AND THE FEARLESS LEADER OF TEAM-D JAKE JENSEN.
+ * NO I"M NOT DEDICATING IT TO Richard Stallman
+ * SORRY Nathaniel maybe next time. :(
+ */
+
+
+
+@TeleOp(name="Template: Iterative OpMode", group="Iterative Opmode")  // @Autonomous(...) is the other common choice
+@Disabled
+public class BasicOpMode_PartyProphetEdition extends OpMode {
+    /* Declare OpMode members. */
+    private ElapsedTime runtime = new ElapsedTime();
+
+    //MIGHT HAVE TO CHANGE TYPE OF MOTOR I HAVE NO IDEA WHAT YOUR TEAMS ARE USING  -MUHAMMAD (A.K.A The Party Prophet)
+    private DcMotor leftMotor = null;
+    private DcMotor rightMotor = null; //The value is null to represent the void where my heart used to be. (Also because you need to initialize in the init class not here)
+
+
+    /*
+     * Code to run ONCE when the driver hits INIT
+     */
+    @Override
+    public void init() {
+        telemetry.addData("Status", "Initialized");
+
+        /* eg: Initialize the hardware variables. Note that the strings used here as parameters
+         * to 'get' must correspond to the names assigned during the robot configuration
+         * step (using the FTC Robot Controller app on the phone).
+         */
+        leftMotor = hardwareMap.dcMotor.get("left_drive");
+        rightMotor = hardwareMap.dcMotor.get("right_drive");//JAKE JENSON Approves(used to get the Motor which you name though the app)
+
+        // eg: Set the drive motor directions:
+        // Reverse the motor that runs backwards when connected directly to the battery
+        leftMotor.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
+        rightMotor.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
+        telemetry.addData("Status", "Initialized");
+        //partyTime();
+        // uncomment at your own risk-MUHAMMAD (A.K.A The Party Prophet)
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+     */
+    @Override
+    public void init_loop() {
+    }
+
+    /*
+     * Code to run ONCE when the driver hits PLAY (or one of the pit crew people #pitcrewlivesmatter)
+     */
+    @Override
+    public void start() {
+        runtime.reset();
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+     */
+    @Override
+    public void loop() {
+        telemetry.addData("Status", "Running: " + runtime.toString());
+
+        // eg: Run wheels in tank mode (note: The joystick goes negative when pushed forwards)
+        leftMotor.setPower(-gamepad1.left_stick_y);
+        rightMotor.setPower(-gamepad1.right_stick_y);
+
+
+    }
+
+    /*
+     * Code to run ONCE after the driver hits STOP
+     */
+    @Override
+    public void stop() {
+        //You usually use this to reset values -MUHAMMAD (A.K.A The Party Prophet)
+    }
+
+    //In memory of Jake Jensen.
+    //DIDN'T UNDERSTAND ANY OF THE CODE WELL..... YELL AT CALVIN NOT ME. TBH MY COMMENTING WAS ON POINT
+
+
+    public void partyTime() {
+        int counterStart = 10;
+        for (int timer = 10; timer>0; timer--) {
+                telemetry.addData("PARTY TIME STARTS IN", "T-MINUS" + String.valueOf(timer));
+        }
+    }
+}
+
+
+
